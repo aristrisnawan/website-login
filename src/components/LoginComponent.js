a49872074772fc50e06472b3c8c1ca139519ebb9
aristrisnawan <aristrisnawan35@gmail.com>
2022-04-08 14:57:12 +0700

    Setting TailwindCSS

---
+++ src/components/LoginComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function LoginComponent() {
+  return (
+    <div className="w-full h-screen bg-orange-200">
+      <h1 className="text-3xl font-bold">WELCOME TO MY WEBSITE</h1>
+    </div>
+  );
+}
